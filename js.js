5389418c403a5b4637d7444717f1e43a5069f29b
Codermate <154111348+codermate01@users.noreply.github.com>
2024-03-01 12:11:28 -0300

    Add choice function and game logic
    Added computerSelection function and also the logic of the game

--- js.js
+++ js.js
@@ -1,1 +1,39 @@
-console.log("holo")
+function getComputerChoice(){
+    var choice = Math.floor(Math.random()*3);
+    switch(choice){
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissors";
+            break;
+    }
+}
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        return "Tie"
+    }else if(playerSelection === "Rock"){
+        if(computerSelection === "Paper"){
+            return `You lose!${computerSelection} beats ${playerSelection}`
+        }else{
+            return `You win! ${playerSelection} beats ${computerSelection}`
+        }
+    }else if(playerSelection === "Paper"){
+        if(computerSelection === "Scissors"){
+            return `You lose!${computerSelection} beats ${playerSelection}`
+        }else{
+            return `You win! ${playerSelection} beats ${computerSelection}`
+        }
+    }else{
+        if(computerSelection === "Rock"){
+            return `You lose!${computerSelection} beats ${playerSelection}`
+        }else{
+            return `You win! ${playerSelection} beats ${computerSelection}`
+        }
+    }
+}
+console.log(playRound("Scissors",getComputerChoice()))
+// console.log(getComputerChoice())
